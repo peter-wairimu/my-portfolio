593b0f099b24b3ce26836d972764ba2fcfba9d43
peter-wairimu <peter.wairimu@student.moringaschool.com>
2021-11-26 16:01:56 +0300

    modifiy page

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+            $(".navbar").addClass("sticky");
+        }else{
+            $(".navbar").removeClass("sticky");
+        }
+    });
+    // toogle menu script
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active")
+        $('.menu-btn i').toggleClass("active")
+    });
+});
+
+// typed animation script
+var typed = new Typed(".typing",{
+    strings:["Programmer.", "Full Stack Developer.", "Software Engineer.", "Designer.", "Freelancer."],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+
+var typed = new Typed(".typing2",{
+    strings:["Programmer.", "Full Stack Developer.", "Software.", "Designer.", "Freelancer."],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+
 const navToggler = document.querySelector(".nav-toggler");
 
 navToggler.addEventListener("click",navToggle);
